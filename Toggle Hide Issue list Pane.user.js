39e45bbec549ab7d26361903cc62ae68786901ec
faethen <cordrim@gmail.com>
2023-08-04 15:03:35 -0700

    Add files via upload

--- Toggle Hide Issue list Pane.user.js
+++ Toggle Hide Issue list Pane.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
-// @name         Toggle Hide JQL Search Panel
+// @name         Toggle Hide Issue list Pane
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  Toggle hiding and showing the JQL search pane using a keypress (default is ]). Change the first * in the match to your JIRA URL.
+// @description  Toggle hiding and showing the issue list pane using a keypress (default is \). Change the first * in the match to your JIRA URL.
 // @author       faethen
 // @match        https://*/*?*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
 (function() {
@@ -16,12 +15,12 @@
     var $ = window.jQuery;
     var $hidden = false;
     $(document).on('keydown', function(e) {
-        if (e.which == 221 && $hidden == false) {
-            $('.issue-search-header').hide();
+        if (e.which == 220 && $hidden == false) {
+            $('.list-results-panel').hide();
             $hidden = true;
             window.dispatchEvent(new Event('resize'));
-        } else if (e.which == 221) {
-            $('.issue-search-header').show();
+        } else if (e.which == 220) {
+            $('.list-results-panel').show();
             $hidden = false;
             window.dispatchEvent(new Event('resize'));
         }
